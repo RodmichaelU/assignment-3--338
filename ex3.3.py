986ee698320afae7ffb7c3590113c5d744142301
Rodmichael <113862691+RodmichaelU@users.noreply.github.com>
2023-03-11 14:50:54 -0700

    Update ex3.3.py

--- ex3.3.py
+++ ex3.3.py
@@ -1,7 +1,13 @@
+"""
+Prints when the capacity changes
 import sys
 
 lst = []
 prev_capacity = sys.getsizeof(lst)
+
+print("Number of elements in my_list: " , len(lst))
+print("Allocated memory for my_list: ", sys.getsizeof(lst))
+print()
 
 for i in range(64):
     lst.append(i)
@@ -9,3 +15,27 @@ for i in range(64):
     if capacity != prev_capacity:
         print(f"Capacity changed from {prev_capacity} bytes to {capacity} bytes at size {len(lst)}")
         prev_capacity = capacity
+        
+"""
+
+
+"""
+Prints the capacity for all entries
+"""
+import sys
+
+lst = []
+capacity = sys.getsizeof(lst)
+print(f"Initial capacity: {capacity}")
+
+for i in range(1, 64):
+    lst.append(i)
+    new_capacity = sys.getsizeof(lst)
+    if new_capacity != capacity:
+        print(f"Capacity changed from {capacity} to {new_capacity} at length {len(lst)}")
+        capacity = new_capacity
+    else:
+        print(f"Capacity is {capacity} at length {len(lst)}")
+
+
+
